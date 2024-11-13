1fe66f8de756b1ec7ca4d8039136a55106724db6
Miauu-N <nicoromero.6@hotmail.com>
2024-11-13 08:47:55 -0300

    pre imagen

---
+++ src/Vista/Grafica/Partida.java
@@ -0,0 +1,16 @@
+package Vista.Grafica;
+
+import javax.swing.*;
+
+public class Partida {
+    private JPanel panel1;
+    private JPanel pMesa;
+    private JPanel pJugador;
+    private JPanel pRival1;
+    private JPanel pRival2;
+    private JPanel pRival3;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+}
